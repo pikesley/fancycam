566f166829ff4a717d75a311937c97ebb4cb094d
pikesley <sam.pikesley@gmail.com>
2020-06-27 19:55:45 +0100

    Enable Sass

--- fancycam/fancycam/__init__.py
+++ fancycam/fancycam/__init__.py
@@ -1,5 +1,21 @@
+import sass
 from flask import Flask
+from sassutils.wsgi import SassMiddleware
 
 app = Flask(__name__)
+
+
+app.wsgi_app = SassMiddleware(
+    app.wsgi_app,
+    {
+        "fancycam": {
+            "sass_path": "static/sass",
+            "css_path": "static/css",
+            "wsgi_path": "/static/css",
+            "strip_extension": True,
+        }
+    },
+)
+
 
 from fancycam import routes  # isort:skip;pylint:disable=C0413,R0401
