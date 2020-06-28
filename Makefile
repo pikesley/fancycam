4925fe44bdc1acd06fd9ca285fc57f87d1bb2c1d
pikesley <sam.pikesley@gmail.com>
2020-06-28 19:36:12 +0100

    Only run `sass` in the Docker dev environment

--- Makefile
+++ Makefile
@@ -9,6 +9,7 @@ build:
 run:
 	docker run \
 		--name ${PROJECT} \
+		--env PLATFORM=docker \
 		--volume $(shell pwd)/${PROJECT}:/opt/${PROJECT} \
 		--volume $(shell echo ${HOME})/.ssh:/root/.ssh \
 		--publish 5000:5000 \
--- fancycam/fancycam/__init__.py
+++ fancycam/fancycam/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 import sass
 from flask import Flask
 from sassutils.wsgi import SassMiddleware
@@ -5,18 +7,19 @@ from sassutils.wsgi import SassMiddleware
 app = Flask(__name__)
 TEMPLATES_AUTO_RELOAD = True
 
-
-app.wsgi_app = SassMiddleware(
-    app.wsgi_app,
-    {
-        "fancycam": {
-            "sass_path": "static/sass",
-            "css_path": "static/css",
-            "wsgi_path": "/static/css",
-            "strip_extension": True,
-        }
-    },
-)
+if "PLATFORM" in os.environ:
+    if os.environ["PLATFORM"] == "docker":
+        app.wsgi_app = SassMiddleware(
+            app.wsgi_app,
+            {
+                "fancycam": {
+                    "sass_path": "static/sass",
+                    "css_path": "static/css",
+                    "wsgi_path": "/static/css",
+                    "strip_extension": True,
+                }
+            },
+        )
 
 
 from fancycam import routes  # isort:skip;pylint:disable=C0413,R0401
