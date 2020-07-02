52c8d8539dedd171d3448d06ae6aa9c423315054
pikesley <sam.pikesley@gmail.com>
2020-07-02 21:34:43 +0100

    Add cargo-culted video stream
    From https://blog.miguelgrinberg.com/post/video-streaming-with-flask

--- fancycam/fancycam/routes.py
+++ fancycam/fancycam/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import Response, render_template
 
-from fancycam import app
+from fancycam import app, camera
 
 
 @app.route("/")
@@ -8,3 +8,17 @@ from fancycam import app
 def index():
     """The noddiest route."""
     return render_template("index.html", title="FancyCam", bulma_version="0.9.0")
+
+@app.route("/video_feed")
+def video_feed():
+    """Route for video feed."""
+    return Response(
+        gen(camera.Camera()), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
+
+def gen(cam):
+    """Yield frames from camera."""
+    while True:
+        frame = cam.get_frame()
+        yield b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+
